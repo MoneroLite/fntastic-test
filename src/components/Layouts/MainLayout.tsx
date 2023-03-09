0ed7e1c9b3ba0702b2ef6587b1310118ce1122c3
MoneroLite <73478668+MoneroLite@users.noreply.github.com>
2023-03-09 19:04:15 +0500

    update styles

--- src/components/Layouts/MainLayout.tsx
+++ src/components/Layouts/MainLayout.tsx
@@ -8,7 +8,7 @@ interface IMainLayout {
 export const MainLayout = ({ children }: IMainLayout) => {
   return (
     <div className="flex w-screen h-screen">
-      <div className="h-full w-20 bg-slate-700">
+      <div className="h-full w-20 bg-neutral-800">
         <ServerList />
       </div>
       <div className="w-full h-full">{children}</div>
--- src/components/Layouts/ServerLayout.tsx
+++ src/components/Layouts/ServerLayout.tsx
@@ -1,22 +1,35 @@
-import React from "react";
-import { UsersList } from "../index";
+import React, { useState } from "react";
+import { Profile, UsersList, HeaderChat, ChannelList, Chat } from "../index";
 import "./serverLayout.scss";
 
 export const ServerLayout = () => {
+  const [usersSidebar, setUsersSidebar] = useState(true);
   return (
     <div className="flex h-full max-h-full">
       <div className="w-1/5 h-full">
-        <div className="h-12 bg-gray-600">server header</div>
-        <div className=" channel__list">chnl list</div>
-        <div className="h-16 bg-gray-600">user info</div>
+        <div className="h-12 bg-neutral-700 border-b border-r border-neutral-800">
+          <p className="font-bold text-neutral-100 p-2">Cупер сервер</p>
+        </div>
+        <div className="bg-neutral-700 channel__list overflow-y-scroll">
+          <ChannelList />
+        </div>
+        <div className="h-16 bg-neutral-700 border-t border-neutral-800">
+          <Profile />
+        </div>
       </div>
       <div className="w-full">
-        <div className="w-full h-12 bg-slate-300">chat header</div>
+        <div className="w-full h-12 bg-neutral-700">
+          <HeaderChat userList={usersSidebar} setUserList={setUsersSidebar} />
+        </div>
         <div className="flex wrap__chat">
-          <div className="w-full bg-slate-700">chat</div>
-          <div className="w-1/5 bg-slate-500">
-            <UsersList />
+          <div className="w-full bg-neutral-600">
+            <Chat />
           </div>
+          {usersSidebar && (
+            <div className="w-1/5 bg-neutral-800 overflow-y-scroll">
+              <UsersList />
+            </div>
+          )}
         </div>
       </div>
     </div>
