6e4f35d2c85414f9eb7a74aa89842e2562907b2e
MoneroLite <73478668+MoneroLite@users.noreply.github.com>
2023-03-09 19:04:41 +0500

    update imports

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,7 @@ export { ServerLayout } from "./Layouts/ServerLayout";
 export { MainLayout } from "./Layouts/MainLayout";
 export { ServerList } from "./ServersList/ServerList";
 export { UsersList } from "./UsersList/UsersList";
+export { Profile } from "./Profile/Profile";
+export { HeaderChat } from "./HeaderChat/HeaderChat";
+export { ChannelList } from "./ChannelList/ChannelList";
+export { Chat } from "./Chat/Chat";
